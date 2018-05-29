3e5dee8d1d1540afbfeaa5f2275bef71555242c1
Brandon Green <brandon@brandonleeproductions.com>
2018-05-29 23:13:56 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,15 +7,15 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(name)
-      collection = []
-      name.each do |x|
-        collection << x.capitalize + "!"
+      collection = [] # local var array 
+      name.each do |x| # Each name do the following block of code
+        collection << x.capitalize + "!" # Shoveling on capitalized x and "!" to the collection array 
       end
       collection
 end
 
 def long_planeteer_calls(word)
-   word.any? {|i| i.length > 4}
+   word.any? {|i| i.length > 4} # Checks each word if they are longer than 4
 end
 
 def find_the_cheese# code an argument here
